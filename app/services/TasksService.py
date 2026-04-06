b844bb8d673b9788fe077a30b005d3aea0e95e4b
simon <accounts@simonpluess.ch>
2026-04-06 17:17:38 +0200

    Setup: taskservice

---
+++ app/services/TasksService.py
@@ -0,0 +1,10 @@
+from app.api.TasksApi import TasksApi
+from app.models.Task import Task
+
+class TasksService:
+    def __init__(self, tasks_api: TasksApi):
+        self.tasks_api = tasks_api
+
+    def get_all_tasks(self) -> list[Task]:
+        data = self.tasks_api.get_tasks()
+        return [Task(board_id=item["board_id"], id=item["id"] ,title=item["title"]) for item in data]
