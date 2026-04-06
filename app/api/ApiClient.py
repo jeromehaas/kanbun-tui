0aaa049dbf95efc92ae06c6e4189ff1549a1b18e
simon <accounts@simonpluess.ch>
2026-04-06 18:18:20 +0200

    Refactor: added some better comments

--- app/api/ApiClient.py
+++ app/api/ApiClient.py
@@ -5,11 +5,17 @@ class ApiClient:
         self.base_url = base_url.rstrip("/")
         self.headers = {"Content-Type": "application/json"}
 
+        # TOKEN IS USED ADD IT TO HEADER
         if token:
             self.headers["Authorization"] = f"Bearer {token}"
 
+    # GET FUNCTION
     async def get(self, path: str):
+
+        # BUILD THE URL-PATH
         url = f"{self.base_url}/{path.lstrip('/')}"
+
+        # MAKE API-CALL AND RETURN DATA AS JSON
         async with httpx.AsyncClient(headers=self.headers) as client:
             response = await client.get(url)
             response.raise_for_status()
