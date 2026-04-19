f28ed55a73c9ffedf606506b883ab02d553d4a77
simon <accounts@simonpluess.ch>
2026-04-19 11:11:30 +0200

    Refactor: naming and comments

--- app/screens/DeleteBoardScreen.py
+++ app/screens/DeleteBoardScreen.py
@@ -24,21 +24,25 @@ class DeleteBoardScreen(Screen):
         # CREATE SERVICES
         self.boards_api = BoardsApi(api_client)
 
+    # COMPOSE ALL ELEMENTS
     def compose(self):
         yield Grid(
     Label("Delete Board", id="title"),
              Label(f"Do you want to delete the board {self.board_name}?", id="question"),
             Button("Cancel", variant="primary", id="cancel"),
-             Button("OK",variant="primary" ,id="delete"), id="dialog"
+             Button("OK",variant="primary" ,id="ok"), id="dialog"
         )
 
+    # METHOD: BUTTON PRESSED EVENT
     async def on_button_pressed(self, event: Button.Pressed):
 
-        if event.button.id == "delete":
+        # IF OK BUTTON
+        if event.button.id == "ok":
             await self.boards_api.delete_board_by_id(self.board_id)
             self.app.pop_screen()
             await self.app.screen.reload_screen()
 
+        # IF CANCEL BUTTON
         if event.button.id == "cancel":
             self.app.pop_screen()
             await self.app.screen.reload_screen()
